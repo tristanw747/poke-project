6df66abe5651d2db57f9df6b01d4488ccd3d8567
Tristan <tristanw747@gmail.com>
2023-04-18 18:57:46 -0700

    removed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-// import { BrowserRouter } from "react-router-dom";
 
-// const el = document.getElementById('root');
-// const root = ReactDOM.createRoot(el);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
     <App />
-
 );
-
-
-{/* <React.StrictMode>
-    <BrowserRouter></BrowserRouter>
-
-    </BrowserRouter>
-  </React.StrictMode> */}
